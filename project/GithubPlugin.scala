d84423432f961f047a78b0a09ad992d85b449428
Justin Kaeser <justin.kaeser@jetbrains.com>
2017-04-21 10:23:03 -0500

    autoImport, setting and task

--- project/GithubPlugin.scala
+++ project/GithubPlugin.scala
@@ -15,5 +15,11 @@ object GithubPlugin extends AutoPlugin {
   override def requires: Plugins = JvmPlugin
   override def trigger = allRequirements
 
+  object autoImport {
+    val githubToken = settingKey[String]("GitHub API token. Don't commit this in your project!")
+    val githubCreateProject = taskKey[Unit]("Create a github project and push this repository.")
+  }
+
+
 
 }
